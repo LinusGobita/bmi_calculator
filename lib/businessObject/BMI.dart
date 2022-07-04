cc8a499953bc9e2e8b77f1f30f539513f31d87d0
pas <root@wibblywobb.li>
2022-07-04 20:14:55 +0200

    add username to DB

--- lib/businessObject/BMI.dart
+++ lib/businessObject/BMI.dart
@@ -7,24 +7,28 @@ class BMI {
   final int id;
   final double bmiScore;
 
+  String userName;
+
   BMI({
     required this.id,
+    required this.userName,
     required this.bmiScore,
   });
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'bmiScore': bmiScore};
+    return {'id': id, 'bmiScore': bmiScore, 'userName': userName};
   }
 
   @override
   String toString() {
-    return 'BMI{id: $id, bmiScore: $bmiScore}';
+    return 'BMI{id: $id, bmiScore: $bmiScore, userName: $userName}';
   }
 
   factory BMI.fromMap(Map<String, dynamic> json) {
     return BMI(
       id: json['id'],
-      bmiScore: json['bmiScore']
+      bmiScore: json['bmiScore'],
+        userName: json['userName']
     );
   }
 
